2f53092a922ccfdd09ce27128493fd9f7cf42dfa
TaHkucT <EcTb_PpobuTuE@TakTochHo.cum>
2022-04-29 19:05:16 +0300

    added tests(again)

--- Storage/tests.py
+++ Storage/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class IndexTest(TestCase):
+    def Set_up(self):
+        client = Client()
+        self.response = client.get(reverse('index'))
+
+    def main_responce(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def main_contest(self):
+        self.assertEqual(self.response.context[])
